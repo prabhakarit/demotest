863f263a765bb90a0404ad9c063f55fbc62df1a8
Prabhakar Pratim Borah <4507982+prabhakarit@users.noreply.github.com>
2021-07-12 22:57:17 +0530

    added tests

--- src/test/java/com/ApplicationIntegrationTests.java
+++ src/test/java/com/ApplicationIntegrationTests.java
@@ -1,7 +1,12 @@
 package com;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import com.example.test.Manager;
 import com.example.test.context.Context;
 import com.example.test.context.Data;
+import com.example.test.entities.IField;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
@@ -34,5 +39,21 @@ class ApplicationIntegrationTests {
         context.setCsvFormatFile(datafiles.getFormatFilePath());
         context.setCsvDataFile(datafiles.getDataFilePath());
         Manager.instance.run(context);
+	}
+
+    @Test
+	void testFormatGettingList() throws Exception {
+        // Running for sample2
+		Context context = new Context();
+        Data datafiles = new Data("sample2");
+        context.setCsvFormatFile(datafiles.getFormatFilePath());
+        context.setCsvDataFile(datafiles.getDataFilePath());
+        List<IField> listOfFieldDefinitions = IField.gList(context);
+        assertEquals(7, listOfFieldDefinitions.size());
+        datafiles = new Data("sample1");
+        context.setCsvFormatFile(datafiles.getFormatFilePath());
+        context.setCsvDataFile(datafiles.getDataFilePath());
+        listOfFieldDefinitions = IField.gList(context);
+        assertEquals(6, listOfFieldDefinitions.size());
 	}
 }
